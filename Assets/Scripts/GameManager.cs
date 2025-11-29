ea3272f3f983c27d1d7a126435e75c875c722e90
MEmilia01 <mariana_cardozo@euneiz.com>
2025-11-29 20:55:00 +0100

    Gamemanager llamado a las cartas y objetos vacios con gamemanager y factory

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Drawing.Inspector.PropertyDrawers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,29 +9,40 @@ public class GameManager : MonoBehaviour
     private static GameManager instance;
     
 
-    //instancia de las variables de las escenas
-    string prinivel;
-    string segnivel;
-    string trinivel;
-    //
-
+    [SerializeField] private LetterFactory factory;
+    [Header("Configuración Casas")]
+    [SerializeField][Range(1, 5)] public int num = 1; // con estos numeros haremos que salgan mas o menos cartas
+    //se generan todas en el mismo objeto de la factory, asi que solo aparezca una
+    [Header("Casas Configuration")]
+    [SerializeField] private List<HouseData> casasvecinos = new List<HouseData>(); //la lista de las casas
+    [System.Serializable]
+    public class HouseData
+    {
+        public Letter.Destination destination;
+        public string casasnombre = "Casa";//para ver si funciona, en el debug
+    }
+    
+    //prueba si hace las cartas
+    /*
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Asignarlettersacasas();
+        }
+    }
+    */
 
     private void Awake()
     {
-        instance = this;
-    }
-    void Start()
-    {
-        //en las variables hay que poner los nombre de las escenas
-        prinivel = SceneManager.GetActiveScene().name;
-        prinivel = "";
-        segnivel = SceneManager.GetActiveScene().name;
-        segnivel = "";
-        trinivel = SceneManager.GetActiveScene().name;
-        trinivel = "";
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(gameObject); }
     }
 
-    
     public static GameManager Instance
     {
         get
@@ -43,25 +56,30 @@ public class GameManager : MonoBehaviour
             else { return instance; }
         }
     }
+    //crear cartas y asignarle un enum, aunq esto ya se hace desde letterfactori
+    //puntos, con las cartas y el evento
 
-    // se necesita que cuando se entregen la carta los enemigos tengan un check
-
-
-    //para cambiar de escena
-    //tendra que recivir una señal para cambiar de escenas
-    public void Firtslevel() 
+    public void Asignarlettersacasas()
     {
-        SceneManager.LoadScene(prinivel);
-    }
-    public void SecondLevel() 
-    {
-        Debug.Log("Cambio a nivel2");
-        SceneManager.LoadScene(segnivel);
-    }
-    public void ThirdLevel() 
-    {
-        Debug.Log("Cambio a nivel3");
-        SceneManager.LoadScene(trinivel);
-    }
-
+        
+        // hace que haya la misma cantidad de casas que en num
+        while (casasvecinos.Count > num)
+        {
+            casasvecinos.RemoveAt(casasvecinos.Count - 1);
+        }
+        while (casasvecinos.Count < num)
+        {
+            casasvecinos.Add(new HouseData { casasnombre = $"Casa{casasvecinos.Count + 1}" });
+        }
+        // Crea cartas SOLO para 'num' casas
+        for (int i = 0; i < num; i++)
+        {
+            HouseData house = casasvecinos[i];
+            factory.GenerateLetter(house.destination);
+            Debug.Log($"Carta {i + 1} para {house.casasnombre}");
+            
+        }
+        
+    } 
 }
+
