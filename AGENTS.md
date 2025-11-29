06d27b0e6606c5252ada4e0652f7dd77ef71137d
Aimar <aimar_elarre@euneiz.com>
2025-11-29 18:12:33 +0100

    Add
    escena DDOL y SceneController

--- AGENTS.md
+++ AGENTS.md
@@ -2,13 +2,13 @@
 - Project name: Jam_Euneiz_2025
 - Unity version: Unity 6000.0.59f2
 - Active scene:
-  - Name: Aimar
+  - Name: DDOL
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController
   - Layers:
     - Default, TransparentFX, Ignore Raycast, ground, Water, UI, CameraColision
 - Active game object:
-  - Name: Suelo (2)
+  - Name: SceneManager
   - Tag: Untagged
-  - Layer: ground
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,39 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as SceneController;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        CargarEscena(1);
+    }
+    protected virtual void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+    public void CargarEscena(int scen)
+    {
+
+        SceneManager.LoadScene(scen);
+    }
+    public void CerrarEscena(int scen)
+    {
+        SceneManager.UnloadSceneAsync(scen);
+    }
+}
